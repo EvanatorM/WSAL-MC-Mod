dafb6d5771f7280e8179327d1c3a0b6ea7d45604
WSAL Evan <evan@millers6.com>
2024-10-17 15:33:33 -0400

    Created the mod

---
+++ src/client/java/com/wsalmc/WSALMCModClient.java
@@ -0,0 +1,10 @@
+package com.wsalmc;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class WSALMCModClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
